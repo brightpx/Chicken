c738f9d8d6c8accf0eded3eed76bddd8206708ae
brightpx <dechsaradecho@gmail.com>
2026-08-01 12:53:14 +0700

    fix error

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ChickenTypesModule } from '../chicken-types/chicken-types.module';
 import { CustomersModule } from '../customers/customers.module';
+import { SupabaseModule } from '../common/supabase.module';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 
 @Module({
-  imports: [ChickenTypesModule, CustomersModule],
+  imports: [ChickenTypesModule, CustomersModule, SupabaseModule],
   controllers: [OrdersController],
   providers: [OrdersService],
   exports: [OrdersService],
